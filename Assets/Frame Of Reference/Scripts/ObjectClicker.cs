ae958324a34091d4cee558e310fb59ab6e52bade
rhyssaunders44 <78834755+rhyssaunders44@users.noreply.github.com>
2021-11-30 16:11:44 +1100

    Stuff

--- Assets/Frame Of Reference/Scripts/ObjectClicker.cs
+++ Assets/Frame Of Reference/Scripts/ObjectClicker.cs
@@ -11,8 +11,6 @@ public class ObjectClicker : MonoBehaviour
 
     /// <summary> The speed the player will jump between people at. </summary>
     [SerializeField] private float jumpMovingSpeed = 5;
-
-    /// <summary> The speed the player will rotate during jumps between people. </summary>
     [SerializeField] private float jumpRotationSpeed = 3;
 
     /// <summary>This will get all the components in the hierarchy. </summary>
@@ -21,13 +19,13 @@ public class ObjectClicker : MonoBehaviour
         cam = GetComponent<Camera>();
     }
 
-    /// <summary> Mainly called to get the clicker or relaad the scene. </summary>
+    /// <summary> Mainly called to get the clicker or reload the scene. </summary>
     private void Update()
     {
         // Checks the clicker.
         RayCastClicker();
 
-        // If there is a desination for the player to move to.
+        // If there is a destination for the player to move to.
         if (lerpTo != null)
         {
             MoveToTarget();
@@ -68,45 +66,31 @@ public class ObjectClicker : MonoBehaviour
     /// <summary> Reloads the current scene. </summary>
     private void ReloadScene()
     {
-        // Gets the current Scene Number.
         Scene scene = SceneManager.GetActiveScene();
-        // Reloads this scene.
         SceneManager.LoadScene(scene.name);
     }
 
     /// <summary> This is used to check if something is clicked to move to it or go to the next level. </summary>
     private void RayCastClicker()
     {
-        // Make a Ray for where the mouse is.
+
         Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-        // Draw this ray in scene to a maximum of 100m.
-        Debug.DrawRay(ray.origin, ray.direction * 100f, Color.magenta);
-        // When the Main Click is pressed down
         if (Input.GetMouseButtonDown(0))
         {
-            // Makes a RaycastHit.
             RaycastHit hit;
-            // If the Raycast hits an object.
             if (Physics.Raycast(ray, out hit))
             {
-                // If the player clicks on an object named finish.
                 if (hit.transform.CompareTag("Finish"))
                 {
-                    // This will either go to the next level or main menu if it is the last level.
                     NextLevel();
                 }
-
-                // This will print the tag of the object.
-                Debug.Log(hit.transform.tag);
-                // This goes through the children of the said gameObject and finds the one which has the "CameraTarget" on it.
+                
+                
                 CameraTarget hitCam = hit.transform.GetComponentInChildren<CameraTarget>();
-                // If it does get a target.
+                
                 if (hitCam != null)
                 {
-                    // Will make the "lerpTo" target = the transform of the new GameObject.
                     lerpTo = hitCam.transform;
-                    // Deattaches this GameObject from the current parrent and makes it not have any parrent.
-                    // It does this by making the parent null and bringing it back to the base layer.
                     transform.parent = null;
                 }
             }
